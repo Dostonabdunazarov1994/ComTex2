33e9112e991f732c94b636535995c724bc212561
Acer <abdunazarov.doston1001@gmail.com>
2021-06-29 16:44:46 +0500

    ComTex2

---
+++ ComTexLab/ComTexLab/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ComTexLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(622, 213);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(208, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "№";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Ф.И.О";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Группа";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Рейтинг";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 355);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Абдуназаров";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ComTexLab/ComTexLab/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace ComTexLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            var E = new Excel.Application();
+            E.Visible = true;
+            E.Workbooks.Open(openFileDialog1.FileName);
+            var Sh = E.ActiveSheet;
+            Sh.Name = "COM";
+            string[] arr = new string[4];
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.Rows.Clear();
+            int r = 0;
+            do
+            {
+                r++;
+                arr[0] = Sh.Cells[r + 1, 1].Text;
+                if (arr[0] == "") break;
+                arr[1] = Sh.Cells[r + 1, 2].Text;
+                arr[2] = Sh.Cells[r + 1, 3].Text;
+                arr[3] = Sh.Cells[r + 1, 4].Text;
+                dataGridView1.Rows.Add(arr[0], arr[1], arr[2], arr[3]);
+            } while (true);
+            r--; 
+            var Ch = E.Charts.Add();
+            Ch.Location(Excel.XlChartLocation.xlLocationAsObject, "COM");
+            Ch = E.ActiveChart;
+            Ch.SeriesCollection(1).Delete();
+            Ch.HasTitle = 1;
+            Ch.HasLegend = false;
+            Ch.ChartTitle.Text = "Успеваемость студентов";
+            Ch.Axes(1).HasTitle = true;
+            Ch.Axes(1).AxisTitle.Text = "Студенты";
+            Ch.Axes(2).HasTitle = true;
+            Ch.Axes(2).AxisTitle.Text = "Рейтинг";
+            var W = new Word.Application();
+            W.Visible = false;
+            var D = W.Documents.Add();
+            var t = W.Selection;
+            t.Font.Bold = -1;
+            t.Font.Size = 24;
+            t.TypeText("Данные о студентах.");
+            t.TypeParagraph();
+            t.Font.Bold = 0;
+            t.Font.Size = 14;
+            for (int k = 1; k <= r; k++)
+            {
+                t.TypeText(k.ToString() + " : ");
+                t.TypeText(Sh.Cells[k + 1, 2].Value.ToString() + " ");
+                t.TypeText(Sh.Cells[k + 1, 3].Text + " ");
+                t.TypeText(Sh.Cells[k + 1, 4].Value.ToString());
+                t.TypeParagraph();
+            }
+            t.TypeParagraph();
+            t.TypeText("Всего " + r.ToString() + " студентов.");
+            t.TypeParagraph();
+            Ch.ChartArea.Select();
+            Ch.ChartArea.Copy();
+            t.TypeParagraph();
+            t.Paste();
+            D.SaveAs(Path.GetDirectoryName(openFileDialog1.FileName) + "\\Test");
+            W.Quit(); W = null;
+            E.Quit(); E = null;
+        }
+    }
+}
